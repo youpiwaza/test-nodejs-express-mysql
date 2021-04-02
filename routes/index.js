629e3a65738a5ceec616eb8bc5d3180de8fe09a7
Max <masamune.code@gmail.com>
2021-04-02 11:40:01 +0200

    ✨ADD: premiere requête

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,20 @@ var router = express.Router();
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host    : 'localhost',
-  user    : 'root',
-  password: ''
+  host    : 'localhost'
+  ,user    : 'root'
+  ,password: ''
+  ,database: 'classicmodels'
 });
 
 db.connect(function(err) {
   if (err) throw err;
-  console.log("Connecté à la base de données MySQL!");
+  console.log('Connecté à la base de données MySQL!');
+  db.query('SELECT * FROM customers', function (err, result) {
+    if (err) throw err;
+    // Tableau d'objets, contenant les lignes de la bdd
+    console.log(result);
+  });
 });
 
 /* GET home page. */
